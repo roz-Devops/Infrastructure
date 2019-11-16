ba806fed8201dd7fee7fab0d04a1105b7e9417cf
roz-Devops <55594261+roz-Devops@users.noreply.github.com>
2019-11-17 01:30:41 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,6 +1,6 @@
 variable "aws_region" { default = "us-east-2" }
 variable "count_var" { default = 1 }
-variable "cred_file" { default = "/var/jenkins_home/aws_keys/credentials" }
+variable "cred_file" { default = "~/.aws/credentials" }
 variable "instanceType" {}
 variable "aws_vpc" {}
 variable "instanceName" {}
