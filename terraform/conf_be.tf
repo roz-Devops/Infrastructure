2f5615d27bfd6394614644c9b40c0977a5790ced
roz-Devops <55594261+roz-Devops@users.noreply.github.com>
2019-11-21 05:24:45 +0200

    Update conf_be.tf

--- terraform/conf_be.tf
+++ terraform/conf_be.tf
@@ -3,8 +3,7 @@ terraform {
         organization = "devops-proj" # org name from step 2.
         workspaces {
             name = "my-app" # name for your app's state.
-        }
-        key    = "terraform.tfstate"
-        region = "us-east-2"
+            }
+      
     }
 }
