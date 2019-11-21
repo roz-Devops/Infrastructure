dda260c62b31a1b44a17553e607f530b475b5b0c
roz-Devops <55594261+roz-Devops@users.noreply.github.com>
2019-11-21 10:31:43 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,17 +3,17 @@ variable "count_var" { default = "1" }
 variable "cred_file" { default = "~/.aws/credentials" }
 variable "instanceType" { default = "t2.micro" }
 
-variable "aws_vpc_id" { default = "string1" }
+variable "aws_vpc_id" { type = "string" }
 variable "envName" { default = "string1" }
 variable "publicip" { default = true }
 variable "releaseVersion" { default = "1.0.0" }
-variable "securitygroup" { default = "string1" }
+variable "securitygroup" { type = "string" }
 output "ov" {
   value = "{var.securitygroup}"
 }
 variable "subnetName" { default = "sub"}
 variable "serviceName" {
- default = "string"
+ default = "string1"
 }
 variable "serviceVersion" {
   default = "serviceVersion"
