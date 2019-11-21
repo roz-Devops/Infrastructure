52348c83680472051b48180ff2f42f8cf6b93508
roz-Devops <55594261+roz-Devops@users.noreply.github.com>
2019-11-21 10:46:46 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,13 +1,13 @@
-variable "aws_region" { default = "us-east-2" }
-variable "count_var" { default = "1" }
+variable "aws_region" { }
+variable "count_var" { }
 variable "cred_file" { default = "~/.aws/credentials" }
 variable "instanceType" { default = "t2.micro" }
 
-variable "aws_vpc_id" { type = "string" }
+variable "aws_vpc_id" { } 
 variable "envName" { default = "string1" }
 variable "publicip" { default = true }
 variable "releaseVersion" { default = "1.0.0" }
-variable "securitygroup" { type = "string" }
+variable "securitygroup" { }
 output "ov" {
   value = "{var.securitygroup}"
 }
