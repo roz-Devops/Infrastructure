5e1076123e599921a38e91d7742d28be4d512c17
roz-Devops <55594261+roz-Devops@users.noreply.github.com>
2019-11-21 09:29:25 +0200

    Update conf_be.tf

--- terraform/conf_be.tf
+++ terraform/conf_be.tf
@@ -1,5 +1,6 @@
 terraform {
     backend "remote" {
+        hostname = "app.terraform.io"
         organization = "devops-proj" # org name from step 2.
         workspaces {
             name = "my-app" # name for your app's state.
